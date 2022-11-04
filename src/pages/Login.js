afb4be7a0cb0a2cb22e6fd4318cf6aa3ae3efbb3
hildelio <hildelio@gmail.com>
2022-11-04 15:31:57 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends Component {
+  state = {
+    user: '',
+    isDisabled: true,
+    isLoading: false,
+    isRedirecting: false,
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    const MIN_LENGTH = 3;
+    this.setState({
+      [name]: value,
+    }, () => {
+      const { user } = this.state;
+      if (user.length >= MIN_LENGTH) {
+        this.setState({
+          isDisabled: false,
+        });
+      } else {
+        this.setState({
+          isDisabled: true,
+        });
+      }
+    });
+  };
+
+  handleButton = async () => {
+    const { user } = this.state;
+    this.setState({ isLoading: true });
+    await createUser({ name: user });
+    this.setState({ isRedirecting: true });
+  };
+
   render() {
+    const { user, isDisabled, isLoading, isRedirecting } = this.state;
     return (
-      <div data-testid="page-login">
-        <p>Login</p>
-      </div>
+      <Route exact path="/">
+        {isLoading ? <Loading /> : (
+          <div data-testid="page-login">
+            <input
+              name="user"
+              type="text"
+              value={ user }
+              onChange={ this.handleChange }
+              data-testid="login-name-input"
+            />
+            <button
+              type="button"
+              data-testid="login-submit-button"
+              disabled={ isDisabled }
+              onClick={ this.handleButton }
+            >
+              Entrar
+            </button>
+          </div>
+        )}
+        {isRedirecting && <Redirect to="/search" />}
+      </Route>
     );
   }
 }
